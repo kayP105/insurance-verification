3271f4865fbbf1fee163c43073d1e81d6cb823a0
Karunya Paul <122207391+kayP105@users.noreply.github.com>
2025-12-29 10:39:52 +0530

    Update API key to use placeholder
    Replaced the actual API key with a placeholder for security.

--- backend/data/rag_chatbot.py
+++ backend/data/rag_chatbot.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path='../.env') 
 
 GEMINI_SECRET_KEY = os.environ.get("GEMINI_SECRET_KEY")
-genai.configure(api_key="AIzaSyDTEB9aZej85I1sVGLofgQU_17Nc8aBF3A")  
+genai.configure(api_key="xxxxxxxx")  
 
 def build_retriever():
     backend_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
